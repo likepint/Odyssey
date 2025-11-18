5c8afbb792c259b19dee073c46d224ce9d104103
likepint <jsuuu96@kakao.com>
2025-11-18 21:03:39 +0900

    Update CHelpers header
    GetAsset, GetClass 템플릿 함수 추가

--- Source/Odyssey/Public/Utilities/CHelpers.h
+++ Source/Odyssey/Public/Utilities/CHelpers.h
@@ -4,7 +4,23 @@
 
 class ODYSSEY_API CHelpers
 {
-public:
+public:	
+	template<typename T>
+	static void GetAsset(TObjectPtr<T>& OutObject, FString InPath)
+	{
+		ConstructorHelpers::FObjectFinder<T> asset(*InPath);
+		if (asset.Succeeded())
+			OutObject = asset.Object;
+	}
+	
+	template<typename T>
+	static void GetClass(TSubclassOf<T>& OutClass, FString InPath)
+	{
+		ConstructorHelpers::FClassFinder<T> asset(*InPath);
+		if (asset.Succeeded())
+			OutClass = asset.Class;
+	}
+
 	template<typename T>
 	static T* LoadSynchronous(const TSoftObjectPtr<T>& InPath)
 	{
